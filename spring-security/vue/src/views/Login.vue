02fa07ca6cc080a28cc1653225e5bb6e376f984c
dalufanjiadan <dalufanjiadan@qq.com>
2019-12-18 15:17:15 +0800

    test login.vue

---
+++ spring-security/vue/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login">
+        sdfasdfasfd
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Login",
+    components: {
+    }
+};
+</script>
