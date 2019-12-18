9dd0f1ec5ff5823dc6899c9043fe315865b65a13
dalufanjiadan <dalufanjiadan@qq.com>
2019-12-18 14:17:15 +0800

    npm  run build outputDir路径 更改到后端static 下

---
+++ spring-security/vue/vue.config.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+    outputDir: path.resolve(__dirname, "../src/main/resources/static"),
+    // 好像用不到
+    // assetsDir: path.resolve(__dirname, "../src/main/resources/static")
+};
