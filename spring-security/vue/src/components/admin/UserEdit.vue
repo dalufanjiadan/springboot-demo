bf1faf43fc25c6fd1ece24c56b824ed35edc9bf8
dalufanjiadan <dalufanjiadan@qq.com>
2019-12-26 21:52:04 +0800

    a

--- spring-security/vue/src/components/admin/UserEdit.vue
+++ spring-security/vue/src/components/admin/UserEdit.vue
@@ -1,36 +1,73 @@
 <template>
     <section>
-        <div class="block">
-            <b-checkbox v-model="checkboxGroup"
-                native-value="Silver">
-                Silver
-            </b-checkbox>
-            <b-checkbox v-model="checkboxGroup"
-                native-value="Flint">
-                Flint
-            </b-checkbox>
-            <b-checkbox v-model="checkboxGroup"
-                native-value="Vane">
-                Vane
-            </b-checkbox>
-            <b-checkbox v-model="checkboxGroup"
-                native-value="Billy" disabled>
-                Billy
-            </b-checkbox>
-        </div>
-        <p class="content">
-            <b>Selection:</b>
-            {{ checkboxGroup }}
-        </p>
+        <button class="button is-primary is-medium" @click="isComponentModalActive = true">
+            Launch component modal
+        </button>
+
+        <b-modal
+            :active.sync="isComponentModalActive"
+            has-modal-card
+            trap-focus
+            aria-role="dialog"
+            aria-modal
+        >
+            <modal-form v-bind="formProps"></modal-form>
+        </b-modal>
     </section>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                checkboxGroup: ['Flint']
+const ModalForm = {
+    props: ["email", "password"],
+    template: `
+            <form action="">
+                <div class="modal-card" style="width: auto">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Login</p>
+                    </header>
+                    <section class="modal-card-body">
+                        <b-field label="Email">
+                            <b-input
+                                type="email"
+                                :value="email"
+                                placeholder="Your email"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Password">
+                            <b-input
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="Your password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-checkbox>Remember me</b-checkbox>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button" type="button" @click="$parent.close()">Close</button>
+                        <button class="button is-primary">Login</button>
+                    </footer>
+                </div>
+            </form>
+        `
+};
+
+export default {
+    components: {
+        ModalForm
+    },
+    data() {
+        return {
+            isComponentModalActive: false,
+            formProps: {
+                email: "evan@you.com",
+                password: "testing"
             }
-        }
+        };
     }
+};
 </script>
--- spring-security/vue/vue.config.js
+++ spring-security/vue/vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
     outputDir: path.resolve(__dirname, "../src/main/resources/static"),
+    runtimeCompiler:true
     // 好像用不到
     // assetsDir: path.resolve(__dirname, "../src/main/resources/static")
 };
