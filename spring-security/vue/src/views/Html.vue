1d4dc983e0b7b98f13a083418bbd02f6cab6b895
dalufanjiadan <dalufanjiadan@qq.com>
2020-01-16 11:42:52 +0800

    vue 使用html 静态资源

--- spring-security/vue/src/views/Html.vue
+++ spring-security/vue/src/views/Html.vue
@@ -3,12 +3,18 @@
         <h1>hello world</h1>
         <h1>hello world</h1>
         <h1>hello world</h1>
-        <div class="section"></div>
+        <a :href="`${publicPath}html/test.html`">href</a>
+        
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            publicPath: process.env.BASE_URL
+        }
+    },
     methods: {
         onTestBtn: function() {
             console.log("hello world");
