5f3dc9f5a723d9b247eef039d414bfed53028986
dalufanjiadan <dalufanjiadan@qq.com>
2020-01-16 16:52:35 +0800

    a

--- spring-security/vue/src/views/Html.vue
+++ spring-security/vue/src/views/Html.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="section">
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
         <a :href="`${publicPath}html/test.html`">href</a>
-        <pre>
 
-            <Prism language="java">
-                int i =10;
-            </Prism>
-        </pre>
-        <pre>
-            
+        <div class="columns">
+            <div class="column is-half">
+                <pre>
+                    <Prism language="html">
+&lt;h1&gt;hello world&lt;/h1&gt;
+&lt;p&gt; fasdfasdfasdfas &lt;/p&gt;
+                    </Prism>
+                </pre>
+            </div>
+            <div class="column  is-half">
+                <h1>hello world</h1>
+                <hr>
+                <p>fasdfasdfasdfas</p>
+            </div>
+        </div>
 
+        <pre>
              <Prism language="js">
 let i = 10;
 let i = 10;
@@ -58,5 +64,8 @@ export default {
     line-height: 1.5;
     /* background-color: aquamarine; */
     font-size: 1em;
+}
+pre{
+    /* padding: 0%; */
 }
 </style>
